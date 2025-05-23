8e60e40905ee4f744abbdd0f1955fdba092114c2
Hwayeon Hong <164862216+ghkdusghd@users.noreply.github.com>
2025-05-23 11:16:43 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,34 +1,35 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
-		
-	public static void main(String[] args) throws Exception, IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] ary = new int[9];
-		int sum = 0;
-		
-		for(int i = 0; i < 9; i++) {
-			ary[i] = Integer.parseInt(br.readLine());
-			sum += ary[i];
-		}
-		
-		br.close();
-		
-		for(int i = 0; i < 8; i++) {
-			for(int j = i+1; j < 9; j++) {
-				if(sum - ary[i] - ary[j] == 100) {
-					ary[i] = 0;
-					ary[j] = 0;
-					Arrays.sort(ary);
-					for(int k = 2; k < 9; k++) {
-						System.out.println(ary[k]);
-					}
-					return;
-				}
-			}	
-				
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] ary = new int[9];
+
+        // 키를 입력받음과 동시에 전체 키를 더해둔다.
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            ary[i] = Integer.parseInt(br.readLine());
+            sum += ary[i];
+        }
+
+        br.close();
+
+        // 아홉 난쟁이 배열의 합계에서 2개를 뺀 값이 100이 되는 경우의 수를 찾는다.
+        for (int i = 0; i < ary.length; i++) {
+            for (int j = i + 1; j < ary.length; j++) {
+                if (sum - ary[i] - ary[j] == 100) {
+                    ary[i] = 0;
+                    ary[j] = 0;
+
+                    // 오름차순 정렬 후 출력
+                    Arrays.sort(ary);
+                    for (int k = 2; k < ary.length; k++) {
+                        System.out.println(ary[k]);
+                    }
+                    return;
+                }
+            }
+        }
+    }
 }
