6353d25044f01e1aea62a55da1ff5768b70b15fd
Hwayeon Hong <164862216+ghkdusghd@users.noreply.github.com>
2025-05-27 11:36:15 +0900

    [Bronze III] Title: 집 주소, Time: 100 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Bronze/1284. 집 주소/집 주소.java
+++ 백준/Bronze/1284. 집 주소/집 주소.java
@@ -1,43 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        List<String> list = new ArrayList<>();
         while (true) {
             String N = br.readLine();
             if (N.equals("0")) break;
-            list.add(N);
-        }
+            int sum = N.length() + 1; // 양 끝 여백 + 숫자간 여백 더함
 
-        for (int i = 0; i < list.size(); i++) {
-            // 1. 양끝 여백을 더함
-            int sum = 2;
-
-            // 2. 숫자 사이 여백을 더함
-            String num = list.get(i);
-            sum += num.length() - 1;
-
-            // 3. 숫자의 너비를 더함
-            char[] chars = num.toCharArray();
-            for (int j = 0; j < chars.length; j++) {
-                if (chars[j] == '1') {
+            for (int i = 0; i < N.length(); i++) {
+                char num = N.charAt(i);
+                if (num == '1') {
                     sum += 2;
-                } else if (chars[j] == '0') {
+                } else if (num == '0') {
                     sum += 4;
                 } else {
                     sum += 3;
                 }
             }
 
-            System.out.println(sum);
+            sb.append(sum).append("\n");
         }
-
-
+        System.out.println(sb);
     }
 }
