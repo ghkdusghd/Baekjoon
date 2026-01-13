537014ca013480d639f3015a351e470b67a33a16
Hwayeon Hong <164862216+ghkdusghd@users.noreply.github.com>
2026-01-13 20:00:11 +0900

    [Bronze II] Title: 2009년, Time: 88 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/2948. 2009년/2009년.java
+++ 백준/Bronze/2948. 2009년/2009년.java
@@ -3,25 +3,16 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         int[] months = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] weeks = {"Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"};
+        String[] weeks = {"Wednesday","Thursday","Friday","Saturday","Sunday","Monday","Tuesday"};
 
         int D = read();
         int M = read();
 
-        int count = 0;
         for (int i = 0; i < M; i++) {
-            count += months[i];
+            D += months[i];
         }
-        count += D;
-        int n = count % 7 - 1;
 
-        // ArrayIndexOutOFBounds 에 대한 예외 처리
-        if (n == -1) {
-            System.out.println("Wednesday");
-            return;
-        }
-        
-        System.out.println(weeks[n]);
+        System.out.println(weeks[D%7]);
     }
 
     static int read() throws Exception {
