4f5e2e21e0c3f5bb22a0325a520ab60ec9b7ce23
Hwayeon Hong <164862216+ghkdusghd@users.noreply.github.com>
2026-01-15 14:23:24 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 120 ms, Memory: 15700 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int[] chess = {1, 1, 2, 2, 2, 8};
+        for (int i = 0; i < 6; i++) {
+            System.out.print(chess[i] - read() + " ");
+        }
+    }
+
+    static int read() throws Exception {
+        int c = 0, n = 0;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
+}
